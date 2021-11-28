40c9e9923b082916f1031a3abd94f3d46f0afb1a
Pritam Dhara <65482473+hDmtP@users.noreply.github.com>
2021-11-28 18:25:55 +0530

    added badge.svg

--- my_stats.py
+++ my_stats.py
@@ -14,6 +14,8 @@ contribs_2021 = user.contributions(
 
 sc = '''
 <h2 align="center">Hello there<img src="https://user-images.githubusercontent.com/88626025/135751180-b3d128a5-ba6f-496d-a6d0-1503b568ee88.gif" width="30px"></h2>
+
+![Tests](https://github.com/hdmtp/hdmtp/actions/workflows/main.yml/badge.svg)
 <h3 align="center" margin=30px>
 ''' + f"\"{choice()}\"" + '''
 </h3>
